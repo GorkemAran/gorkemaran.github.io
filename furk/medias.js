b21d574276a404cd853540e5f4a5a4d88c88a186
GorkemAran <77414785+GorkemAran@users.noreply.github.com>
2023-10-08 03:44:10 +0300

    Add files via upload

---
+++ furk/medias.js
@@ -0,0 +1,61 @@
+import images1 from "./images1"
+import images2 from "./images2"
+import images3 from "./images3"
+import images4 from "./images4"
+import images5 from "./images5"
+import images6 from "./images6"
+import images7 from "./images7"
+import images8 from "./images8"
+import images9 from "./images9"
+import images10 from "./images10"
+import images11 from "./images11"
+import images12 from "./images12"
+import images13 from "./images13"
+import images14 from "./images14"
+import images15 from "./images15"
+import images16 from "./images16"
+import images17 from "./images17"
+import images18 from "./images18"
+import images19 from "./images19"
+import images20 from "./images20"
+import images21 from "./images21"
+import images22 from "./images22"
+import images23 from "./images23"
+import images24 from "./images24"
+import images25 from "./images25"
+import images26 from "./images26"
+import images27 from "./images27"
+import images28 from "./images28"
+import images29 from "./images29"
+
+export const medias = [
+     images1,
+    images2,
+    images3,
+    images4,
+    images5,
+    images6,
+    images7,
+    images8,
+    images9,
+    images10,
+     images11,
+     images12,
+     images13,
+     images14,
+     images15,
+     images16,
+     images17,
+     images18,
+     images19,
+     images20,
+     images21,
+     images22,
+     images23,
+     images24,
+     images25,
+     images26,
+     images27,
+     images28,
+     images29,
+]  
